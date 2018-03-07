37691192b7cd066a234b11cdcb186c37d8c41180
Yomaira Escano <yjescano@gmail.com>
2018-03-07 19:54:01 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,14 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(post_params)
+	  @post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(post_params)
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
@@ -29,8 +29,8 @@ class PostsController < ApplicationController
 
   private
 
-    def post_params
-      params.require(:post.permit(:title, :description))
+    def post_params(*args)
+      params.require(:post).permit(*args)
     end
-    
+
 end
